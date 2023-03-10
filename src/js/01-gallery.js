0fb7d8003676b805c5fae8e33ada7d697451eed4
Dominik Szajbler <110773381+dominikszajbler@users.noreply.github.com>
2023-03-10 22:52:49 +0100

    gallery ready

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,29 @@
+// Opisany w dokumentacji
+import SimpleLightbox from "simplelightbox";
+// Dodatkowy import stylów
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+const galleryItem = galleryItems
+	.map(
+		(item) =>
+			`<div class="gallery__item">
+    <a class="gallery__link" href="${item.original}">
+      <img
+        class="gallery__image"
+        src="${item.preview}"
+        data-source="${item.original}"
+        alt="${item.description}"
+      />
+    </a>
+  </div>`,
+	)
+	.join("");
+gallery.innerHTML = galleryItem;
+new SimpleLightbox(".gallery a", {
+	captionDelay: 250,
+  captionsData: "alt",
+});
